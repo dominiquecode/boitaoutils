015a7edb7045e0f2b704d2e3b2616bcdf6e3e3d1
git_user <admin@tradom.ca>
2017-07-06 12:02:21 -0400

    Ajout de création superuser

--- testapp/management/commands/superuser.py
+++ testapp/management/commands/superuser.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 
 class Command(BaseCommand):
 
+    message_ok = "L'admin a bien été créé. N'oubliez pas de modifier le mot de passe par défaut!"
+    message_no = "L'admin existe déjà!"
+
     def handle(self, *args, **options):
         if not User.objects.filter(username='admin').count():
             try:
@@ -13,6 +16,6 @@ class Command(BaseCommand):
                 raise CommandError("Problème de création de l'admin")
 
             user.save()
-            self.stdout.write(self.style.SUCCESS("L'admin est créé"))
+            self.stdout.write(self.style.SUCCESS(self.message_ok))
         else:
-            self.stdout.write(self.style.ERROR("L'admin existe déjà!"))
+            self.stdout.write(self.style.ERROR(self.message_no))
